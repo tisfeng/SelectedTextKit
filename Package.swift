dc96e395dd5a45c79ae40c5475047c4016cb652c
tisfeng <tisfeng@gmail.com>
2024-10-25 21:03:15 +0800

    feat: add getSelectedText

--- Package.swift
+++ Package.swift
@@ -1,21 +1,35 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SelectedTextKit",
+    platforms: [
+        .macOS(.v10_15)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SelectedTextKit",
-            targets: ["SelectedTextKit"]),
+            targets: ["SelectedTextKit"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/tmandry/AXSwift.git", from: "0.3.0"),
+        .package(url: "https://github.com/s1ntoneli/AXSwiftExt", from: "0.1.0"),
+        .package(url: "https://github.com/jordanbaird/KeySender", from: "0.0.5"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SelectedTextKit"),
+            name: "SelectedTextKit",
+            dependencies: [
+                "AXSwift",
+                "AXSwiftExt",
+                "KeySender",
+            ]
+        ),
         .testTarget(
             name: "SelectedTextKitTests",
             dependencies: ["SelectedTextKit"]
---
+++ Sources/SelectedTextKit/SharedUtilities.swift
@@ -0,0 +1,40 @@
+//
+//  SharedUtilities.swift
+//  Easydict
+//
+//  Created by tisfeng on 10/15/24.
+//  Copyright © 2024 izual. All rights reserved.
+//
+
+import AXSwift
+import Foundation
+
+// MARK: - SharedUtilities
+
+/// Shared utilities for objc and swift.
+@objc
+public class SharedUtilities: NSObject {
+    static func isAccessibilityEnabled() -> Bool {
+        checkIsProcessTrusted()
+    }
+}
+
+/// Sync poll task, if task is true, return true, else continue polling.
+///
+/// - Warning: ⚠️ This method will block the current thread, only use when necessary.
+func pollTask(
+    _ task: @escaping () -> Bool,
+    every interval: TimeInterval = 0.005,
+    timeout: TimeInterval = 0.1,
+    timeoutCallback: @escaping () -> () = {}
+) {
+    let startTime = Date()
+    while Date().timeIntervalSince(startTime) < timeout {
+        if task() {
+            return
+        }
+        Thread.sleep(forTimeInterval: interval)
+    }
+    timeoutCallback()
+    logInfo("pollTask timeout call back")
+}
